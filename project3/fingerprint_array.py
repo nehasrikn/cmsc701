783af5daee023bb8ce9d94384d437d1c2288179b
Neha Srikanth <nehasrik@umd.edu>
2023-05-12 03:52:12 -0400

    eval

--- project3/fingerprint_array.py
+++ project3/fingerprint_array.py
@@ -3,6 +3,7 @@ import random
 import bbhash
 import hashlib
 import time
+import os
 import numpy as np
 
 def hash_string(s, bits):
@@ -15,21 +16,19 @@ def hash_string(s, bits):
 def get_last_n_bits_value(byte_string, n):
     return int.from_bytes(byte_string, "big") & ((1 << n) - 1)
 
-def construct_mphf_and_query(strings, expected_hit_rate, bits_to_use):
+def construct_mphf_and_query(strings, mixture_rate, bits_to_use):
     """Samples k strings from strings and partitions them into two sets, 
     one of size k and one of size k_prime.
     """
     random.seed(42)
-
-    keys = random.sample(strings, len(strings))
+    pivot = len(strings) // 2
+    keys = random.sample(strings, len(strings)) # shuffle the strings
     
-    k_set = keys[:int(len(keys) * expected_hit_rate)]
-    k_prime_set = keys[int(len(keys) * expected_hit_rate):]
+    k_set = keys[:pivot]
+    k_prime_set = keys[pivot:]
     
-    
-    query_set = random.sample(k_set, int(len(k_set) * expected_hit_rate))
-    query_set.extend(random.sample(k_prime_set, int(len(k_prime_set) * (1 - expected_hit_rate))))
-
+    offset = int((1 - mixture_rate) * pivot)
+    query_set = keys[offset:offset + pivot]
 
     uint_hashes = [hash_string(s, 16) for s in k_set]
 
@@ -46,6 +45,8 @@ def construct_mphf_and_query(strings, expected_hit_rate, bits_to_use):
         
     false_positives = 0
     false_positives_fingerprint = 0
+    denom = 0
+
     start_time = time.time()
 
     for item in query_set:
@@ -56,21 +57,57 @@ def construct_mphf_and_query(strings, expected_hit_rate, bits_to_use):
             false_positives += 1
         if item not in k_set and ((lookup is not None) and (fingerprint == 1)):
             false_positives_fingerprint += 1
+        
+        if item not in k_set:
+            denom += 1
     
     end_time = time.time()
     elapsed_time = end_time - start_time
 
-    print("Elapsed Time:", elapsed_time, "seconds")
-    print("False positive rate: {}".format(false_positives / len(query_set)))
-    print("False positive rate with fingerprint: {}".format(false_positives_fingerprint / len(query_set)))
+    mph.save('temp.mph')
 
-    return mph
+    # print("Elapsed Time:", elapsed_time, "seconds")
+    # print("False positive rate: {}".format(false_positives / denom))
+    # print("False positive rate with fingerprint: {}".format(false_positives_fingerprint / denom))
+
+    return elapsed_time, false_positives / denom, false_positives_fingerprint / denom, os.path.getsize('temp.mph')
 
 
 if __name__ == "__main__":
     sg = StringGenerator(min_len=3, max_len=31)
     #print(len(sg.generate_for_length(31)))
-    sample = sg.sample_strings(31, 10000)
 
-    construct_mphf_and_query(sample, 0.8, 16)
+    num_bits = [7, 8, 10, 16]
+    
+    queries = [5000, 10000, 20000]
+
+    mixture = [0.2, 0.4, 0.6, 0.8]
+
+    for b in num_bits:
+        print("**** False positive rate: {} ****".format(b))
+        for q in queries:
+            print("Query size: {}".format(q))
+            sample = sg.sample_strings(31, 2*q)
+
+            times = []
+            fps = []
+            fingerprints = []
+            sizes = []
+
+            for m in mixture:
+                elapsed_time, observed_fp, observed_fp_finger, size = construct_mphf_and_query(sample, 0.8, b)
+
+                times.append(elapsed_time)
+                fps.append(observed_fp)
+                fingerprints.append(observed_fp_finger) 
+                sizes.append(size)
+
+            print("Times: {}".format(times))
+            print("False positives: {}".format(fps))
+            print("False Positives finger: {}".format(fingerprints))
+            print("Size: {}".format(sizes))
+
+            print()
+    
+    print()
         
--- project3/mphf_eval.py
+++ project3/mphf_eval.py
@@ -2,6 +2,7 @@ from string_generator import StringGenerator
 import random
 import bbhash
 import time
+import os
 
 def hash_string(s, bits):
     """Hashes a string to a bits bit integer.
@@ -10,20 +11,20 @@ def hash_string(s, bits):
     hashed_int = hash_value % (2**bits/2)
     return hashed_int
 
-def construct_mphf_and_query(strings, expected_hit_rate):
+def construct_mphf_and_query(strings, mixture_rate):
     """Samples k strings from strings and partitions them into two sets, 
     one of size k and one of size k_prime.
     """
     random.seed(42)
+    pivot = len(strings) //2
+    keys = random.sample(strings, len(strings)) #shuffle the strings
+    
+    k_set = keys[:pivot]
+    k_prime_set = keys[pivot:]
+    
+    offset = int((1 - mixture_rate) * pivot)
+    query_set = keys[offset:offset + pivot]
 
-    keys = random.sample(strings, len(strings))
-    
-    k_set = keys[:int(len(keys) * expected_hit_rate)]
-    k_prime_set = keys[int(len(keys) * expected_hit_rate):]
-    
-    
-    query_set = random.sample(k_set, int(len(k_set) * expected_hit_rate))
-    query_set.extend(random.sample(k_prime_set, int(len(k_prime_set) * (1 - expected_hit_rate))))
 
     uint_hashes = [hash_string(s, 16) for s in k_set]
 
@@ -33,26 +34,53 @@ def construct_mphf_and_query(strings, expected_hit_rate):
     mph = bbhash.PyMPHF(uint_hashes, len(uint_hashes), num_threads, gamma)
     
     false_positives = 0
+    denom = 0
     start_time = time.time()
 
     for item in query_set:
         lookup = mph.lookup(hash_string(item, 16))
         if lookup is not None and item not in k_set:
             false_positives += 1
+        if item not in k_set:
+            denom += 1
     
     end_time = time.time()
     elapsed_time = end_time - start_time
 
-    print("Elapsed Time:", elapsed_time, "seconds")
-    print("False positive rate: {}".format(false_positives / len(query_set)))
-
-    return mph
+    # print("Elapsed Time:", elapsed_time, "seconds")
+    # print("False positive rate: {}".format(false_positives / denom))
+    
+    mph.save('temp.mph')
+    return elapsed_time, false_positives / denom, os.path.getsize('temp.mph')
 
 
 if __name__ == "__main__":
     sg = StringGenerator(min_len=3, max_len=31)
     #print(len(sg.generate_for_length(31)))
-    sample = sg.sample_strings(31, 10000)
 
-    construct_mphf_and_query(sample, 0.8)
+    queries = [5000, 10000, 20000]
+    mixture = [0.2, 0.4, 0.6, 0.8]
+
+
+    for q in queries:
+        print("Query size: {}".format(q))
+        sample = sg.sample_strings(31, 2*q)
+
+        times = []
+        fps = []
+        sizes = []
+
+        for m in mixture:
+            elapsed_time, observed_fp, size = construct_mphf_and_query(
+                sample, m
+            )  
+            times.append(elapsed_time)
+            fps.append(observed_fp)
+            sizes.append(size) 
+
+        print("Times: {}".format(times))
+        print("False positives: {}".format(fps))
+        print("Sizes: {}".format(sizes))
+        print()
+            
         
